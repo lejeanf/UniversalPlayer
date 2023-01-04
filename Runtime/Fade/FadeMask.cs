c95f153bec823fa372f6878912a6c714f26d9bd7
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-01-04 13:33:15 -0400

    added event to set fadeTime

--- Runtime/Fade/FadeMask.cs
+++ Runtime/Fade/FadeMask.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.HighDefinition;
 using DG.Tweening;
+using UnityEngine.Serialization;
 
 [ExecuteAlways]
 public class FadeMask : MonoBehaviour
 {
     private CustomPassVolume _customPassVolume;
 
+    [FormerlySerializedAs("_inputBinding")]
     [Header("Manual Switch Input")]
     [Tooltip("Input to manually switch the fade state")]
-    [SerializeField] private InputAction _inputBinding = new InputAction();
+    [SerializeField] private InputAction inputBinding = new InputAction();
     [Space(10)]
     [Header("Fade Settings")]
     [SerializeField] private float fadeTime = .2f;
@@ -26,14 +28,18 @@ public class FadeMask : MonoBehaviour
     private static readonly int FadeAlpha = Shader.PropertyToID("_Alpha");
     
     private Material _shaderMaterial;
-    private bool isFaded = false;
+    private bool _isFaded = false;
 
     public delegate void SetColor(Color color);
     public static SetColor SetFadeColor;
     public delegate void SetAlpha(float alpha);
     public static SetAlpha SetFadeAlpha;
+    public delegate void FadeTime(float alpha);
+    public static FadeTime SetFadeTime;
     public delegate void FadeToValue(bool value);
     public static FadeToValue FadeTo;
+    public delegate void FadeToValueInTime(bool value, float fadeTime);
+    public static FadeToValueInTime FadeToInSpecificTime;
 
     private void Awake()
     {
@@ -51,33 +57,46 @@ public class FadeMask : MonoBehaviour
 
     private void OnEnable()
     {   
-        _inputBinding.Enable();
-        _inputBinding.performed += _ => SwitchFadeState();
-        
+        inputBinding.Enable();
+        inputBinding.performed += _ => SwitchFadeState();
+
+        SetFadeTime += ctx => fadeTime = ctx;
         SetFadeColor += ctx => color = ctx;
         SetFadeAlpha += ctx => alpha = ctx;
-        FadeTo += FadeValue;
+        FadeTo += FadeValue; // using bool
+        FadeToInSpecificTime += FadeValue; // using bool + float
     }
-
+ 
     private void OnDisable() => Unsubscribe();
     private void OnDestroy() => Unsubscribe();
 
     void Unsubscribe()
     {
-        _inputBinding.performed -= null;
-        _inputBinding.Disable();
+        inputBinding.performed -= null;
+        inputBinding.Disable();
         
+        SetFadeTime -= ctx => fadeTime = ctx;
         SetFadeColor -= ctx => color = ctx;
         SetFadeAlpha -= ctx => alpha = ctx;
         FadeTo -= FadeValue;
+        FadeToInSpecificTime -= FadeValue;
     }
 
     public void SwitchFadeState()
     {
-        isFaded = !isFaded;
-        FadeValue(isFaded);
+        _isFaded = !_isFaded;
+        FadeValue(_isFaded);
     }
     public void FadeValue(bool value)
+    {
+        float tmpAlpha = value ? 1 : 0;
+        var forwardTween = DOTween.To(
+            () => _shaderMaterial.GetFloat(FadeAlpha),
+            (val) => _shaderMaterial.SetFloat(FadeAlpha, val),
+            tmpAlpha,
+            fadeTime);
+    }
+    public void FadeValue(bool value, float fadeTime)
     {
         float tmpAlpha = value ? 1 : 0;
         var forwardTween = DOTween.To(
