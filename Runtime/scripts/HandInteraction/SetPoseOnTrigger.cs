51a83cc8708576a1ab92e0367e67ba936048c32e
Jean-Francois Robin <jean-francois_robin@uqar.ca>
2023-07-14 16:23:22 -0400

    poseTriggerTweak

--- Runtime/scripts/HandInteraction/SetPoseOnTrigger.cs
+++ Runtime/scripts/HandInteraction/SetPoseOnTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace jeanf.vrplayer
@@ -10,20 +11,16 @@ namespace jeanf.vrplayer
         [SerializeField] private Pose defaultPose;
         [SerializeField] private Pose poseToSet;
 
-        private bool rightHandGrab = false;
-        private bool leftHandGrab = false;
+        [SerializeField] private bool canRightHandPoint = false;
+        [SerializeField] private bool canLeftHandPoint = false;
 
-        [SerializeField] private bool isUsingGrabCheck = true;
 
         private void OnTriggerEnter(Collider other)
         {
             if(!other.GetComponent(typeof(BlendableHand))) return;
             _handPoseManager = (HandPoseManager) other.GetComponentInParent(typeof(Transform)).GetComponentInParent(typeof(HandPoseManager));
-            
-            if ((isUsingGrabCheck && _handPoseManager.HandType == HandType.Left && leftHandGrab) || 
-                (isUsingGrabCheck && _handPoseManager.HandType == HandType.Right && rightHandGrab)) return;
-            
-            if(_handPoseManager) _handPoseManager.ApplyPose(poseToSet);
+
+            if(_handPoseManager.HandType == HandType.Left && canLeftHandPoint || _handPoseManager.HandType == HandType.Right && canRightHandPoint) _handPoseManager.ApplyPose(poseToSet);
         }
 
         private void OnTriggerExit(Collider other)
@@ -31,19 +28,18 @@ namespace jeanf.vrplayer
             if (!other.GetComponent(typeof(BlendableHand))) return;
             _handPoseManager = (HandPoseManager)other.GetComponentInParent(typeof(Transform)).GetComponentInParent(typeof(HandPoseManager));
             
-            if ((isUsingGrabCheck && _handPoseManager.HandType == HandType.Left && leftHandGrab) || 
-                (isUsingGrabCheck && _handPoseManager.HandType == HandType.Right && rightHandGrab)) return;
-            
-            if (_handPoseManager) _handPoseManager.ApplyPose(defaultPose);
+            _handPoseManager.ApplyPose(defaultPose);
         }
 
-        public void SetRightHandState(bool state)
+        public void RightHandIsGrabbing()
         {
-            rightHandGrab = state;
+            canLeftHandPoint = true;
+            canRightHandPoint = false;
         }
-        public void SetLeftHandState(bool state)
+        public void LeftHandIsGrabbing()
         {
-            leftHandGrab = state;
+            canLeftHandPoint = false;
+            canRightHandPoint = true;
         }
     }
 }
