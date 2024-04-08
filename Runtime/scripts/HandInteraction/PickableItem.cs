d23993eee0444b1592e1cefd29f2dd99e9084dd7
Percevent13 <109316101+Percevent13@users.noreply.github.com>
2024-04-08 15:45:16 -0400

    added PickableItem script

---
+++ Runtime/scripts/HandInteraction/PickableItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableItem : ScriptableObject
+{
+    public string name;
+    public string id;
+    public GameObject prefab;
+    
+}
