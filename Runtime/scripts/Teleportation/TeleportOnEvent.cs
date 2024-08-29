ee1f0b7a46e92476754778efb6dad2f47e4cbbe7
Percevent13 <fccharlebois@hotmail.com>
2024-08-29 13:50:56 -0400

    update teleport on event again

--- Runtime/scripts/Teleportation/TeleportOnEvent.cs
+++ Runtime/scripts/Teleportation/TeleportOnEvent.cs
@@ -55,7 +55,7 @@ namespace jeanf.vrplayer
             teleportSubject.transform.rotation = teleportInformation.targetDestination.rotation;
             try
             {
-                teleportSubject.GetComponent<PlayerMovement>().gameObject.SetActive(true);
+                teleportSubject.GetComponentInChildren<PlayerMovement>().gameObject.SetActive(true);
             }
             catch
             {
