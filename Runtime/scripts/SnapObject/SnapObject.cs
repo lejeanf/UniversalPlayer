b4aeba845201e6d93d1723c737baf4ccb00e8a81
Percevent13 <fccharlebois@hotmail.com>
2024-09-17 09:46:32 -0400

    Merge branch 'snap-object'

--- Runtime/scripts/SnapObject/SnapObject.cs
+++ Runtime/scripts/SnapObject/SnapObject.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using jeanf.EventSystem;
@@ -17,7 +18,12 @@ namespace jeanf.vrplayer
         public SnapZone AttachedSnapZone {  get { return attachedSnapZone; }}
         [SerializeField] bool shouldOrientOnSnap;
         public bool ShouldOrientOnSnap { get { return shouldOrientOnSnap; } }
-        private void OnTriggerStay(Collider other)
+        public static event Action<Transform, Vector3> OnSnapMove;
+        public static event Action<Transform, Quaternion> OnSnapRotate;
+        public static event Action<bool> OnSnap;
+
+        //On assigne
+        private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.GetComponent<SnapZone>() != null)
             {
@@ -27,17 +33,70 @@ namespace jeanf.vrplayer
                 {
                     snapPoints.Add(snapPoint);
                 }
-                snapEventChannelSO.RaiseEvent(this.gameObject);
             }
         }
 
+        //On call le snap
+        private void OnTriggerStay(Collider other)
+        {
+            Snap();
+        }
 
+        //On désassigne
         private void OnTriggerExit(Collider other)
         {
             if (other.gameObject.GetComponent<SnapZone>() != null)
             {
                 attachedSnapZone = null;
                 nearestSnapPoint = null;
+            }
+        }
+
+        private void Snap()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask: snapTargetLayer))
+            {
+                OnSnap.Invoke(true);
+                float minDistance = Mathf.Infinity;
+                GameObject refSnapPoint = null;
+                foreach (GameObject snapPoint in SnapPoints)
+                {
+                    float distance = Vector3.Distance(hit.point, snapPoint.transform.position);
+
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+
+                        refSnapPoint = snapPoint;
+                    }
+
+                }
+                if (refSnapPoint != nearestSnapPoint)
+                {
+                    nearestSnapPoint = refSnapPoint;
+                    this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+                    if (shouldOrientOnSnap)
+                    {
+                        try
+                        {
+                            SnapZone snapZone = attachedSnapZone.GetComponent<SnapZone>();
+
+                            OnSnapRotate.Invoke(transform, nearestSnapPoint.transform.rotation);
+                        }
+                        catch
+                        {
+                            Debug.LogWarning("Cannot orient towards organ, not an auscultation zone");
+                        }
+                    }
+                    OnSnapMove.Invoke(transform, nearestSnapPoint.transform.position);
+                }
+            }
+            else
+            {
+                this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePosition;
+                OnSnap.Invoke(false);
             }
         }
     }
