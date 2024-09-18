c508c139dc63d306aa6fe7b5f9d0a92de30550a6
Percevent13 <fccharlebois@hotmail.com>
2024-09-18 14:08:43 -0400

    removed freeze position

--- Runtime/scripts/SnapObject/SnapObject.cs
+++ Runtime/scripts/SnapObject/SnapObject.cs
@@ -85,7 +85,6 @@ namespace jeanf.vrplayer
                 if (refSnapPoint != nearestSnapPoint)
                 {
                     nearestSnapPoint = refSnapPoint;
-                    this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
                     if (shouldOrientOnSnap)
                     {
@@ -103,10 +102,7 @@ namespace jeanf.vrplayer
                     OnSnapMove.Invoke(transform, nearestSnapPoint.transform.position);
                     Debug.Log("Snapping to " + nearestSnapPoint.gameObject.name);
                 }
-                //else
-                //{
-                //    this.GetComponent<Rigidbody>().constraints = ~RigidbodyConstraints.FreezePosition;
-                //}
+
             }
             else
             {
