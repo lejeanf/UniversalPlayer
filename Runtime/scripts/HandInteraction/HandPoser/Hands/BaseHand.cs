4de1a723d3ab824810f66c7652a9247cd7f7bc9c
Percevent13 <fccharlebois@hotmail.com>
2025-01-08 15:49:44 -0500

    removed debug

--- Runtime/scripts/HandInteraction/HandPoser/Hands/BaseHand.cs
+++ Runtime/scripts/HandInteraction/HandPoser/Hands/BaseHand.cs
@@ -80,7 +80,6 @@ public abstract class BaseHand : MonoBehaviour, IDebugBehaviour
         if (handInfo == null) return;
         ApplyFingerRotations(handInfo.fingerRotations);
 
-        Debug.Log("APLLYNG POSE");
         // Position, and rotate, this differs on the type of hand
         //ApplyOffset(handInfo.attachPosition, handInfo.attachRotation);
     }
