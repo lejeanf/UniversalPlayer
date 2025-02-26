bcf0f5e2f3fd4c9563a17b8f40bececbef011115
Percevent13 <fccharlebois@hotmail.com>
2025-02-26 16:13:24 -0500

    added debug

--- Runtime/scripts/Movement/PlayerMovement.cs
+++ Runtime/scripts/Movement/PlayerMovement.cs
@@ -52,6 +52,7 @@ namespace jeanf.vrplayer
 
         private void LateUpdate()
         {
+            Debug.Log("IS CHARACTER GROUNDED" + controller.isGrounded);
             if (isMoving)
             {
                 Move(moveValue);
