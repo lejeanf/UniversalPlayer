81bb333e97acaaf65258e1d463d2a8d7af6649d2
jeanf <robije02@uqar.ca>
2025-03-11 10:22:24 -0400

    added delegate to HandVibration script

--- Runtime/scripts/HandInteraction/HandVibration.cs
+++ Runtime/scripts/HandInteraction/HandVibration.cs
@@ -1,6 +1,5 @@
 using jeanf.EventSystem;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 public class HandVibration : MonoBehaviour
 {
@@ -11,16 +10,45 @@ public class HandVibration : MonoBehaviour
 
     [Header("Listening On")]
     [SerializeField] StringEventChannelSO hapticFeedbackOnSpecificHandSO;
+    public delegate void VibrateHandDelegate(string hand, float amplitude, float duration);
+
+    public VibrateHandDelegate VibrateHand;
     private void OnEnable()
     {
         hapticFeedbackOnSpecificHandSO.OnEventRaised += TriggerHapticFeedback;
+        VibrateHand += TriggerHapticFeedback;
     }
 
     private void OnDisable()
     {
         hapticFeedbackOnSpecificHandSO.OnEventRaised += TriggerHapticFeedback;
+        VibrateHand -= TriggerHapticFeedback;
     }
     private void TriggerHapticFeedback(string hand)
+    {
+        switch (hand)
+        {
+            case "Right":
+                rightHandHapticImpulse.SendHapticImpulse(amplitude, duration);
+                break;
+            case "Left":
+                leftHandHapticImpulse.SendHapticImpulse(amplitude, duration);
+                break;
+        }
+    }
+    private void TriggerHapticFeedback(string hand, float duration)
+    {
+        switch (hand)
+        {
+            case "Right":
+                rightHandHapticImpulse.SendHapticImpulse(amplitude, duration);
+                break;
+            case "Left":
+                leftHandHapticImpulse.SendHapticImpulse(amplitude, duration);
+                break;
+        }
+    }
+    private void TriggerHapticFeedback(string hand, float amplitude, float duration)
     {
         switch (hand)
         {
