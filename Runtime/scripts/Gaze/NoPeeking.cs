8251a986d80c1e3900fbecb5c09b402a6d8eb1c0
Robin Jean-François <robije02@uqar.ca>
2026-02-17 13:04:49 -0500

    fix: fade update issue in urp

--- Runtime/scripts/Gaze/NoPeeking.cs
+++ Runtime/scripts/Gaze/NoPeeking.cs
@@ -35,6 +35,11 @@ namespace jeanf.universalplayer
             }
 
             if (_fadeState == _fadeStateLastValue) return;
+
+            var fadeType = _isSceneLoading ? FadeMask.FadeType.Loading : FadeMask.FadeType.HeadInWall;
+            FadeMask.FadeValue(_fadeState, fadeType);
+
+            if (isDebug) Debug.Log($"{fadeType} fade changed to: {_fadeState}");
             
             FadeMask.FadeValue(_fadeState, FadeMask.FadeType.HeadInWall);
             if (isDebug) Debug.Log($"HeadInWall collision fade changed to: {_fadeState}");
