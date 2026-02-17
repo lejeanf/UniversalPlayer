cd9a023fb3d93fec02c3d5f61c1049d6a7c9b4a5
Robin Jean-François <robije02@uqar.ca>
2026-02-17 17:12:58 -0500

    fixed fade for teleport

--- Runtime/scripts/Teleportation/TeleportOnEvent.cs
+++ Runtime/scripts/Teleportation/TeleportOnEvent.cs
@@ -1,9 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using jeanf.EventSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
-using System.Collections;
+
 namespace jeanf.universalplayer
 {
     public class TeleportOnEvent : TeleportEventListener, IDebugBehaviour
@@ -19,9 +18,33 @@ namespace jeanf.universalplayer
         [SerializeField] private Transform cameraOffset;
         [SerializeField] private List<FilterSO> listOfFilters;
 
+        [Header("Fade Settings")]
+        [SerializeField] private float fadeInDuration = 0.2f;
+        [SerializeField] private float fadeOutDuration = 0.2f;
+
         [Header("Broadcasting on:")]
         [SerializeField] private VoidEventChannelSO cameraResetChannel;
-        [SerializeField] private BoolFloatEventChannelSO FadeEventChannel;
+
+        private Coroutine _teleportCoroutine;
+
+        private void OnDestroy()
+        {
+            CleanupCoroutine();
+        }
+
+        private void OnDisable()
+        {
+            CleanupCoroutine();
+        }
+
+        private void CleanupCoroutine()
+        {
+            if (_teleportCoroutine != null)
+            {
+                StopCoroutine(_teleportCoroutine);
+                _teleportCoroutine = null;
+            }
+        }
 
         public void Teleport(TeleportInformation teleportInformation)
         {
@@ -38,11 +61,24 @@ namespace jeanf.universalplayer
             {
                 Debug.Log($"[{gameObject.name}] destination : {teleportInformation.targetDestination.gameObject.name}, objectIsPlayer : {teleportInformation.objectIsPlayer}");
                 Debug.Log($"ObjectToTeleport : {teleportInformation.objectToTeleport.name}");
-            }             
+            }
+
+            CleanupCoroutine();
             
+            _teleportCoroutine = StartCoroutine(TeleportWithFade(teleportInformation));
+        }
+
+        private IEnumerator TeleportWithFade(TeleportInformation teleportInformation)
+        {
+            FadeMask.SetStateLoading();
+            if (_isDebug) Debug.Log("TeleportOnEvent: Fading to black...");
+            
+            yield return new WaitForSeconds(fadeInDuration);
+
             GameObject teleportSubject = teleportInformation.objectIsPlayer
                 ? player
                 : teleportInformation.objectToTeleport.gameObject;
+            
             try
             {
                 teleportSubject.GetComponent<CharacterController>().enabled = false;
@@ -51,20 +87,32 @@ namespace jeanf.universalplayer
             {
                 if (isDebug) Debug.Log("teleportation subject is not player - cannot disable player locomotion for teleportation");
             }
+            
             teleportSubject.transform.position = teleportInformation.targetDestination.position;
-            Debug.Log($"TELEPORT - player position = {teleportSubject.transform.position} && targetDestination.position = {teleportInformation.targetDestination.position}");
             teleportSubject.transform.rotation = teleportInformation.targetDestination.rotation;
+            
+            if (_isDebug) Debug.Log($"TELEPORT - player position = {teleportSubject.transform.position} && targetDestination.position = {teleportInformation.targetDestination.position}");
+            
             try
             {
                 teleportSubject.GetComponent<CharacterController>().enabled = true;
             }
             catch
             {
-                if (isDebug) Debug.Log("teleportation subject is not player - cannot disable player locomotion for teleportation");
+                if (isDebug) Debug.Log("teleportation subject is not player - cannot enable player locomotion after teleportation");
             }
-            if ( teleportInformation.objectIsPlayer ) cameraResetChannel.RaiseEvent();
-            if (_isDebug) Debug.Log( $"[{teleportInformation.targetDestination.gameObject.name}] teleported {teleportSubject.gameObject.name} to {teleportInformation.targetDestination.transform.position} with rotation: {teleportInformation.targetDestination.transform.rotation.eulerAngles}");
+            
+            if (teleportInformation.objectIsPlayer) 
+                cameraResetChannel.RaiseEvent();
 
+            yield return new WaitForSeconds(0.1f);
+
+            FadeMask.SetStateClear();
+            if (_isDebug) Debug.Log("TeleportOnEvent: Fading to clear...");
+            
+            if (_isDebug) Debug.Log($"[{teleportInformation.targetDestination.gameObject.name}] teleported {teleportSubject.gameObject.name} to {teleportInformation.targetDestination.transform.position} with rotation: {teleportInformation.targetDestination.transform.rotation.eulerAngles}");
+            
+            _teleportCoroutine = null;
         }
     }
 }
